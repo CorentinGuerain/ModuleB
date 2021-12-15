9edc261d6eb92f18ceca862a9f94484bef1dff8a
Mildor <80513120+Mildor@users.noreply.github.com>
2021-12-15 16:57:40 +0100

    Add files via upload

---
+++ pointeur/test.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<img data-index="0" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="0" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 0</h2>
+    <button onclick="addToList(0)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="1" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="1" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 1</h2>
+    <button onclick="addToList(1)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="2" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="2" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 2</h2>
+    <button onclick="addToList(2)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="3" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="3" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 3</h2>
+    <button onclick="addToList(3)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="4" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="4" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 4</h2>
+    <button onclick="addToList(4)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="5" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="5" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 5</h2>
+    <button onclick="addToList(5)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="6" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="6" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 6</h2>
+    <button onclick="addToList(6)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div><img data-index="7" src="marker.svg" class="h-[50px] absolute cursor-pointer pointer z-10" alt="map">
+<div data-index="7" class="absolute h-[200px] w-[150px] bg-white hidden pointer-modal z-20 p-3">
+    <h2>Animation 7</h2>
+    <button onclick="addToList(7)" class="py-1 px-2 uppcaser bg-[blue] text-white">Ajouter a l'itinere</button>
+</div>
+</body>
+</html>
