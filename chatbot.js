09df34835fe23d76621246b731c1b3abc3fd4102
CorentinGuerain <coguerain@stpbb.org>
2021-12-15 16:35:23 +0100

    add all animations

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,6 @@
 var première_ouverture = true;
+var $int_position = 1;
+var $repeat_bool = true;
 
 
 
@@ -11,28 +13,71 @@ function bulleCLick(){
         $( "#chatbot" ).fadeOut( 200 );
     }
 
+    console.log(("_"+$int_position));
+
     setTimeout(() => {  if(première_ouverture){
-            animation();
+            animation(("_"+$int_position), $repeat_bool);
             première_ouverture = false;
         }
     }, 200);
     
 }
 
-function animation(){
-
-    $( "#point1" ).fadeIn( 150 );
-    setTimeout(() => { $( "#point2" ).fadeIn( 150 ); }, 150);
-
-    setTimeout(() => { $( "#point3" ).fadeIn( 150 );
-                        $( "#point1" ).fadeOut( 150 ); }, 300);
-
-    setTimeout(() => { $( "#point2" ).fadeOut( 0 ); 
-                        $( "#point3" ).fadeOut( 0 );
-                        $( "#bulle_points" ).fadeOut( 0 );
-                        $( "#text_input" ).fadeIn( 0 );}, 500);
-
-    setTimeout(() => { $( "#test" ).fadeIn( 150 ); }, 500);
-
+function clickButton(){
+    alert("passse");
 }
+
+function animation($position, $repeat){
+
+
+    
+    $( "#point1"+$position ).fadeIn( 150 );
+    $( "#bulle_points"+$position ).fadeIn( 150 );
+    setTimeout(() => { $( "#point2"+$position ).fadeIn( 150 ); }, 150);
+
+    setTimeout(() => { $( "#point3"+$position ).fadeIn( 150 );
+                        $( "#point1"+$position ).fadeOut( 150 ); }, 300);
+
+    setTimeout(() => { $( "#point2"+$position ).fadeOut( 0 ); 
+                        $( "#point3"+$position ).fadeOut( 0 );
+                        $( "#bulle_points"+$position ).fadeOut( 0 );
+                        $( "#text_input"+$position ).fadeIn( 0 );
+                        $( "#btn"+$position ).fadeIn( 0 );}, 500);
+ 
+    setTimeout(() => { $( "#test"+$position ).fadeIn( 150 ); 
+                        if($repeat){
+                            if($int_position==2){
+                                $repeat_bool  =false;
+                            }
+                            $int_position++;
+                            animation(("_"+$int_position), $repeat_bool);
+                        }
+                    }, 500);
+    
+}
+
+
+
+// function animation($position, $repeat){
+
+//     // console.log($position);
+
+    
+//     $( "#point1_1" ).fadeIn( 150 );
+//     setTimeout(() => { $( "#point2_1" ).fadeIn( 150 ); }, 150);
+
+//     setTimeout(() => { $( "#point3_1" ).fadeIn( 150 );
+//                         $( "#point1_1" ).fadeOut( 150 ); }, 300);
+
+//     setTimeout(() => { $( "#point2_1" ).fadeOut( 0 ); 
+//                         $( "#point3_1" ).fadeOut( 0 );
+//                         $( "#bulle_points_1" ).fadeOut( 0 );
+//                         $( "#text_input_1" ).fadeIn( 0 );}, 500);
+ 
+//     setTimeout(() => { $( "#test_1" ).fadeIn( 150 ); }, 500);
+//     // if($repeat){
+//     //     $int_position++;
+//     //     animation(("_"+$int_position), false);
+//     // }
+// }
 
--- index.php
+++ index.php
@@ -14,12 +14,24 @@
 <div id="chatbot_div">
     <div id="chatbot" hidden>
         
-        <div id="bulle_points">
-            <img src="point.png" id="point1" hidden>
-            <img src="point.png" id="point2" hidden>
-            <img src="point.png" id="point3" hidden>
+        <div id="bulle_points_1" class="bulles" hidden>
+            <img src="point.png" id="point1_1" class="points" hidden>
+            <img src="point.png" id="point2_1" class="points" hidden>
+            <img src="point.png" id="point3_1" class="points" hidden>
         </div>
-        <textarea id="test" hidden disabled> Test dedans</textarea>
+        <textarea id="test_1" hidden disabled>Bienvenue ! Laissez-moi vous guider</textarea>
+        <div id="bulle_points_2"  class="bulles" hidden>
+            <img src="point.png" id="point1_2" class="points" hidden>
+            <img src="point.png" id="point2_2" class="points" hidden>
+            <img src="point.png" id="point3_2" class="points" hidden>
+        </div>
+        <textarea id="test_2" hidden disabled>Vous pouvez concevoir votre parcours vous-même, ou vous laisser guidersur un parcours que nous vous avons sélectionné</textarea>
+        <div id="bulle_points_3"  class="bulles" hidden>
+            <img src="point.png" id="point1_3" class="points" hidden>
+            <img src="point.png" id="point2_3" class="points" hidden>
+            <img src="point.png" id="point3_3" class="points" hidden>
+        </div>
+        <button id="btn_3" onclick="clickButton()" hidden>Afficher le parcours recommandé</button>
     </div>
     <img src="bulle.png" id="bulle" onclick="bulleCLick()">
 </div>
